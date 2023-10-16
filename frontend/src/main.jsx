f6fdd51d9d961263281f077c5b7b928f337ca594
Ercan <kadri503394@gmail.com>
2023-10-16 03:46:09 +0300

    feat: added MainLayout added Images required for the project

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import MainLayout from './layouts/MainLayout.jsx'
 
 ReactDOM.createRoot(document.getElementById('ecanivrl')).render(
   <React.StrictMode>
+    <MainLayout>
     <App />
+    </MainLayout>
   </React.StrictMode>,
 )
