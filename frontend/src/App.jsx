59b0e93910a90aad9e914cb472508d75921a2483
Ercan <kadri503394@gmail.com>
2023-10-16 04:18:08 +0300

    feat: added Categories and CategoryItem component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
+import Categories from './components/Categories/Categories';
 import Sliders from './components/Slider/Sliders';
 
 function App() {
   return (
     <>
     <Sliders/>
+    <Categories/>
     </>
   );
 }
