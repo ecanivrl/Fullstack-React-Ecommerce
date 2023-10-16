7edaabfc18592049c3f484466d85b27b2cf9ddb3
Ercan <kadri503394@gmail.com>
2023-10-16 05:02:57 +0300

    feat: added ShopPage component

--- frontend/src/pages/ShopPage.jsx
+++ frontend/src/pages/ShopPage.jsx
@@ -1,8 +1,17 @@
+import React from 'react';
+import Categories from '../components/Categories/Categories';
+import Products from '../components/Products/Products';
+import CampaignSingle from '../components/CampaignSingle/CampaignSingle';
 
 const ShopPage = () => {
   return (
-    <div>ShopPage</div>
-  )
-}
+    <React.Fragment>
+      <Categories />
+      <Products />
+      <CampaignSingle />
+      <Products />
+    </React.Fragment>
+  );
+};
 
-export default ShopPage
+export default ShopPage;
