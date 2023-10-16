f7e1ce63e55a97632e325a447f15ae18b78927e4
Ercan <kadri503394@gmail.com>
2023-10-16 04:26:37 +0300

    feat: added Products and ProductsItem components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css';
 import Categories from './components/Categories/Categories';
+import Products from './components/Products/Products';
 import Sliders from './components/Slider/Sliders';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <>
     <Sliders/>
     <Categories/>
+    <Products/>
     </>
   );
 }
